7c5d021b8bf4faa0dacf752321d3e38f97629eb5
Ajanitone <ajanitone@freenet.de>
2022-07-03 20:49:28 +0200

    initial commit

--- solution.js
+++ solution.js
@@ -1,1 +1,50 @@
 // write your code in this file
+
+console.log("This is an exercise in console logging");
+
+const exercise = "This is the value";
+
+console.log("What is the value of the exercise", exercise);
+
+const firstName = "Kirk";
+
+const lastName = " McDowell";
+
+const age = 46;
+
+const city = "Berlin";
+
+const job = "Webdeveloper";
+
+console.log("firstname ", firstName, "lastname", lastName);
+
+console.log(
+  "firstname",
+  firstName,
+  "lastname",
+  lastName,
+  "age",
+  age,
+  "city",
+  city,
+  "job",
+  job
+);
+console.log("--------------------------------");
+const firstName1 = "Tania";
+
+const lastName1 = " Hensen";
+
+const age1 = 43;
+
+const city1 = "Berlin";
+
+const job1 = "Music-Teacher";
+
+console.log(
+  "" + firstName1,
+  lastName1,
+  "is a " + age1,
+  job1,
+  "who lives in " + city1
+);
